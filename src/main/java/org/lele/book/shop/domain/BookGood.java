5e5636cf24303bf15fe0b1476b2dfecb7b9b5228
shuly <632725532@qq.com>
2017-10-19 17:26:36 +0800

    日志, 模板更新

--- src/main/java/org/lele/book/shop/domain/BookGood.java
+++ src/main/java/org/lele/book/shop/domain/BookGood.java
@@ -1,5 +1,7 @@
 package org.lele.book.shop.domain;
 
+import java.time.LocalDateTime;
+
 /**
  * author  shuly
  * date    17-10-18
@@ -7,4 +9,20 @@ package org.lele.book.shop.domain;
  */
 
 public class BookGood {
+    public int id;
+    public String bookName;
+    public String bookAuthor;
+    public String bookHead;
+    public int price;
+    public int sellCnt;
+    public String simpleDesc;
+    public String tag;
+    public boolean isBlack;
+    public int stock;
+    public String beterPart;
+    public String index;
+    public String detail;
+    public LocalDateTime updateTime;
+    public LocalDateTime createTime;
+
 }
